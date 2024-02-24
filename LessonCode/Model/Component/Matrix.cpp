6881bab37b65461b5ac3db6f8411fb1f21c7194d
LittleLycanLord <vanilla.icecream0919@gmail.com>
2024-02-25 00:16:26 +0800

    SMOLLER

--- LessonCode/Model/Component/Matrix.cpp
+++ LessonCode/Model/Component/Matrix.cpp
@@ -9,9 +9,9 @@ void Matrix::initializeTransformMatrix() {
     this->fTranslate_Y          = 0.0f;
     this->fTranslate_Z          = -2.0f;
 
-    this->fScale_X              = 0.05f;
-    this->fScale_Y              = 0.05f;
-    this->fScale_Z              = 0.05f;
+    this->fScale_X              = 0.005f;
+    this->fScale_Y              = 0.005f;
+    this->fScale_Z              = 0.005f;
 
     this->vecTheta              = glm::vec3(0.f);
     this->vecRotationAxis_X     = glm::vec3(1.0f, 0.0f, 0.0f);
